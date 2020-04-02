bbb69ba41142717f524df8c334e548e9d8a62fff
pro-xiaoy <pro-xiaoy@xiaoy-mbp.local>
2020-04-02 14:44:21 +0800

    完成时间控件的组件化

--- src/compontents/remdaily/remdaily.vue
+++ src/compontents/remdaily/remdaily.vue
@@ -1,22 +1,29 @@
 <template>
   <view>
-    <view class="remdaily_btn">CNM</view>
-    <time-picket :visible="dialogVisible"></time-picket>
+    <view class="remdaily_btn" @click="showtime">CNM</view>
+    <time-picket :visible="dialogVisible" @timeClose="timeClose"></time-picket>
   </view>
 </template>
 <script>
-import timePicket from '../timerpick/timerpick'
+import timePicket from "../timerpick/timerpick";
 export default {
   data() {
     return {
-      dialogVisible: false
-    }
+      dialogVisible: true
+    };
   },
   components: {
-    'timePicket': timePicket
+    timePicket: timePicket
+  },
+  methods: {
+    showtime() {
+      this.dialogVisible = true;
+    },
+    timeClose(val) {
+      this.dialogVisible = val;
+    }
   }
-}
+};
 </script>
 <style lang="scss">
-
 </style>
--- src/compontents/timerpick/timerpick.vue
+++ src/compontents/timerpick/timerpick.vue
@@ -1,10 +1,11 @@
 <template>
   <view>
     <view v-if="dialogVisible" class="timerpick_content animated fadeInUp">
-      <view class="timerpick_cancel" @click="visible=false">取消</view>
+      <view class="timerpick_cancel" @click="emitclose">取消</view>
       <view class="timerpick_sure" @click="suredaily()">确定</view>
       <picker-view
-        indicator-style="height: 50px;line-height:50px" style="width: 100%; height: 300px;"
+        indicator-style="height: 50px;line-height:50px"
+        style="width: 100%; height: 300px;"
         :value="value"
         @change="bindChange"
       >
@@ -23,83 +24,92 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      dialogVisible: {
-        type: Boolean,
-        default: false
-      }
-    },
-    data: function() {
-      const date = new Date();
-      const years = [];
-      const year = date.getFullYear();
-      const months = [];
-      const month = date.getMonth() + 1;
-      const days = [];
-      const day = date.getDate();
-      for (let i = 1980; i <= date.getFullYear(); i++) {
-        years.push(i);
-      }
-      for (let i = 1; i <= 12; i++) {
-        months.push(i);
-      }
-      for (let i = 1; i <= 31; i++) {
-        days.push(i);
-      }
-      return {
-        visible_picket: true,
-        title: "picker-view",
-        years,
-        year,
-        months,
-        month,
-        days,
-        day,
-        value: [9999, month - 1, day - 1],
-      };
-    },
-    onLoad() {
-      console.log('this.years++', this.years)
-      console.log("111", uni.getSystemInfoSync());
-    },
-    methods: {
-      bindChange: function(e) {
-        const val = e.detail.value;
-        this.year = this.years[val[0]];
-        this.month = this.months[val[1]];
-        this.day = this.days[val[2]];
-      },
-      suredaily() {
-        this.visible_picket = false
-      }
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
     }
-  };
-</script>
-<style lang="scss">
-  .timerpick_content {
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-    padding-top: 60rpx;
-    .item,view {
-      line-height: 50px;
+  },
+  data: function() {
+    const date = new Date();
+    const years = [];
+    const year = date.getFullYear();
+    const months = [];
+    const month = date.getMonth() + 1;
+    const days = [];
+    const day = date.getDate();
+    for (let i = 1980; i <= date.getFullYear(); i++) {
+      years.push(i);
     }
-    .timerpick_cancel {
-      color: #ccc;
-      position: absolute;
-      left: 10rpx;
-      top: 15rpx;
-      font-size: 30rpx;
-      line-height: 1;
+    for (let i = 1; i <= 12; i++) {
+      months.push(i);
     }
-    .timerpick_sure {
-      color: rgb(33, 224, 65);
-      position: absolute;
-      right: 10rpx;
-      top: 15rpx;
-      font-size: 30rpx;
-      line-height: 1;
+    for (let i = 1; i <= 31; i++) {
+      days.push(i);
+    }
+    return {
+      dialogVisible: this.visible,
+      title: "picker-view",
+      years,
+      year,
+      months,
+      month,
+      days,
+      day,
+      value: [9999, month - 1, day - 1]
+    };
+  },
+  mounted() {
+  },
+  watch: {
+    visible(val) {
+      console.log("Val+++", val);
+      this.dialogVisible = val
+    }
+  },
+  methods: {
+    bindChange: function(e) {
+      const val = e.detail.value;
+      this.year = this.years[val[0]];
+      this.month = this.months[val[1]];
+      this.day = this.days[val[2]];
+    },
+    suredaily() {
+      this.dialogVisible = false;
+      this.emitclose()
+    },
+    emitclose() {
+      this.$emit('timeClose', false)
     }
   }
+};
+</script>
+<style lang="scss">
+.timerpick_content {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  padding-top: 60rpx;
+  .item,
+  view {
+    line-height: 50px;
+  }
+  .timerpick_cancel {
+    color: #ccc;
+    position: absolute;
+    left: 10rpx;
+    top: 15rpx;
+    font-size: 30rpx;
+    line-height: 1;
+  }
+  .timerpick_sure {
+    color: rgb(33, 224, 65);
+    position: absolute;
+    right: 10rpx;
+    top: 15rpx;
+    font-size: 30rpx;
+    line-height: 1;
+  }
+}
 </style>
